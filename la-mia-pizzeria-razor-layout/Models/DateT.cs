dad3c095fa1a07a72947758f1d78c0a4b10793e6
Luigi9411 <luigi.sciavicco@gmail.com>
2023-04-05 18:29:45 +0200

    first part

--- la-mia-pizzeria-razor-layout/Models/DateT.cs
+++ la-mia-pizzeria-razor-layout/Models/DateT.cs
@@ -2,16 +2,36 @@
 {
     public class DateT
     {
-        public string Format { get; set; }
-
-        public string DateHour { get; set; }
-
         public string getDateHour()
         {
             DateTime timeCurrent = DateTime.Now;
             string format = "yyyy-MM-dd HH:mm:ss";
             string dateHour = timeCurrent.ToString(format);
             return dateHour;
+        }
+
+        public string getDateSevenDaysAgo()
+        {
+            DateTime timeCurrent = DateTime.Now;
+            DateTime timeSevenDaysAgo = timeCurrent.AddDays(-7);
+            string format = "yyyy-MM-dd ";
+            string dateSeven = timeSevenDaysAgo.ToString(format);
+            return dateSeven;
+        }
+
+        public string getDayOfWeekToday()
+        {
+            DateTime timeCurrent = DateTime.Now;
+            string dayOfWeek = timeCurrent.ToString("dddd", new System.Globalization.CultureInfo("it-IT"));
+            return dayOfWeek;
+        }
+
+        public string getDayOfWeekOneYearFromNow()
+        {
+            DateTime timeCurrent = DateTime.Now;
+            DateTime timeOneYearFromNow = timeCurrent.AddYears(1);
+            string dayOfWeekOneYear = timeOneYearFromNow.ToString("dddd", new System.Globalization.CultureInfo("it-IT"));
+            return dayOfWeekOneYear;
         }
     }
 }
