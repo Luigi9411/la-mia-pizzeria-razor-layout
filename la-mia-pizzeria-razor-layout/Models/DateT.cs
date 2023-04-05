71bb68916bc55f99d27dbd09ca4b05fe18c8f812
Luigi9411 <luigi.sciavicco@gmail.com>
2023-04-05 17:51:31 +0200

    esercizio 5

---
+++ la-mia-pizzeria-razor-layout/Models/DateT.cs
@@ -0,0 +1,17 @@
+﻿namespace la_mia_pizzeria_razor_layout.Models
+{
+    public class DateT
+    {
+        public string Format { get; set; }
+
+        public string DateHour { get; set; }
+
+        public string getDateHour()
+        {
+            DateTime timeCurrent = DateTime.Now;
+            string format = "yyyy-MM-dd HH:mm:ss";
+            string dateHour = timeCurrent.ToString(format);
+            return dateHour;
+        }
+    }
+}
